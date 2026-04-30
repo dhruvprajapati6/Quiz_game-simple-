7ad9cc28c4b6f7240af7dd5ce52ef6cc4d858acc
dhruvprajapati6 <dhruvpprajapati2007@gmail.com>
2026-04-30 13:55:32 +0530

    Add files via upload

---
+++ v_project5.py
@@ -0,0 +1,62 @@
+# Create to quiz game to you gate questiond to answers.........
+
+print("🫶 welcome To Quiz Game 👑")
+
+score=0
+
+# q-1
+print("What Is Capital Of India?")
+ans=input("Enter Your Ans:").lower()
+
+if(ans == "dilhi"):
+    print("Correct Ans.....👏")
+    score +=1
+else:
+    print("Ohhh Wrong Ans!!!🤦‍♂️")
+
+# q-2
+
+print("Which Language Mostly Use In Any Formate?")
+ans=input("Enter Your Ans:").lower()
+
+if(ans == "python"):
+    print("Correct Ans.....👏")
+    score +=1
+else:
+    print("Ohhh Wrong Ans!!!🤦‍♂️")
+
+# q-3
+
+print("Python Language Is Beagener To Perfect Language? (yes/no)")
+ans=input("Enter Your Ans:").lower()
+
+if(ans == "yes"):
+    print("Correct Ans.....👏")
+    score += 1
+else:
+    print("Ohhh Wrong Ans!!!🤦‍♂️")
+
+# Q-4
+
+print("What is an IP Address? Full Form Onaly")
+ans=input("Enter Your Ans:").lower()
+
+if(ans == "internet protocol"):
+    print("Correct Ans.....👏")
+    score += 1
+else:
+    print("Ohhh Wrong Ans!!!🤦‍♂️")
+
+# q-5
+
+print("Make Parfect Future In IT Which Basicaly Requied?")
+ans=input("Enter Your Ans:").lower()
+
+if(ans == "skills"):
+    print("Correct Ans.....👏")
+    score += 1
+else:
+    print("Ohhh Wrong Ans!!!🤦‍")
+
+# print score
+print("🐽 Your Score is:",score)
